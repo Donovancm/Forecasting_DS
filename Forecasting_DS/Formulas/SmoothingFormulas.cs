f1b8bcfdd4c09825c0cae0b5731baa1915f1a146
Donovancm <donovan.dcm@gmail.com>
2019-08-11 13:10:20 +0200

    commit 2(testing needed)

---
+++ Forecasting_DS/Formulas/SmoothingFormulas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecasting_DS.Formulas
+{
+    public class SmoothingVariables
+    {
+        public float xT;
+        public float cT;
+        public float l;
+        public float[] sT;
+        public float[] bT;
+        public int current;
+    }
+
+    public class FactorsVariables
+    {
+        public float alpha;
+        public float beta;
+        public float gamma;
+    }
+
+    public class SmoothingFormulas
+    {
+        public SmoothingVariables CreateEmptySmoothingVariables()
+        {
+            return new SmoothingVariables();
+        }
+
+        public FactorsVariables CreateEmptyFactorsVariables()
+        {
+            return new FactorsVariables();
+        }
+
+        public float SmoothLevelCalc(SmoothingVariables levelVariables, FactorsVariables factorsVariables)
+        {
+            // St = alpha * (xT/ (cT- l)) + (1 - alpha) * (sT-1 + bT-1)
+            float sT = factorsVariables.alpha * (levelVariables.xT / (levelVariables.cT - levelVariables.l)) + (1 - factorsVariables.alpha) * (levelVariables.sT[levelVariables.current - 1] + levelVariables.bT[levelVariables.current - 1]);
+
+            return sT;
+        }
+
+        public float SmoothTrendFormulaCalc(SmoothingVariables trendVariables, FactorsVariables factorsVariables)
+        {
+            // Bt = beta * (sT - ST-1) + (1 - beta) * Bt-1
+            float bT = factorsVariables.beta * (trendVariables.sT[trendVariables.current] - trendVariables.sT[trendVariables.current - 1]) + (1 - factorsVariables.beta) * trendVariables.bT[trendVariables.current - 1];
+            return bT;
+        }
+
+        public float SmoothSeasonalFormulaCalc(SmoothingVariables seasonalVariables, FactorsVariables factorsVariables)
+        {
+            // Ct = gamma * (xT / sT) + (1 - gamma) * cT - l
+            float cT = factorsVariables.gamma * (seasonalVariables.xT / seasonalVariables.sT[seasonalVariables.current]) + (1 - factorsVariables.gamma) * seasonalVariables.cT - seasonalVariables.l;
+            return cT;
+        }
+    }
+}
---
+++ Forecasting_DS/Reader/CsvReader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecasting_DS.Reader
+{
+    public class CsvReader
+    {
+
+        public static float[,] GetData()
+        {
+            // init variables
+            float[,] forecastMatrix = CreateMatrix();
+            // setup Offers Matrix = Points
+            SetupOffersMatrix(forecastMatrix);
+            return forecastMatrix;
+        }
+
+        public static float[,] CreateMatrix()
+        {
+            //Skip last row and last column //
+            return new float[60, 8];
+
+        }
+
+        public static void SetupOffersMatrix(float[,] matrix)
+        {
+            List<string> list = new List<string>();
+            using (StreamReader reader = new StreamReader("../../Data/HoltWintersSeasonal.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line); // Add to clusterPoints.
+                    Console.WriteLine(line); // Write to console.
+                }
+            }
+
+            int row = 0;
+            //string[] userArray = { };
+            foreach (var item in list)
+            {
+                // After the the first row comes forcast data
+                if (row > 0)
+                {
+
+                    // set matrix of  forecast csv table
+
+                    for (int i = 0; i <= matrix.GetLength(1)-1; i++)
+                    {
+                        string[] forecastData = item.Split(',');
+                        if (forecastData[i] == "")
+                        {
+                            // if  useroffer is empty string then set 0 in matrix
+                            matrix[row - 1, i] = 0f;
+                        }
+                        else
+                        {
+                            //else useroffer is not empty then set 1 in matrix
+                            matrix[row - 1, i] = float.Parse(forecastData[i]);
+                            
+                        }
+                    }
+                }
+                // set row index to the next line/ the follow up productid of users offers
+                row++;
+            }
+        }
+
+
+    }
+}
